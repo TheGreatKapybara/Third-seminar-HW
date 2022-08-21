e440fbbacb3621f064ca170271c5b7d77e3db75f
theGreatKapybara <oliver.shustar@gmail.com>
2022-08-21 21:33:55 +0500

    HW made

---
+++ task 1/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+string pal = Convert.ToString(number);
+if (pal[0] == pal[4])
+{
+    System.Console.WriteLine("Да");
+}
+else
+{
+    System.Console.WriteLine("Нет");
+}
---
+++ task 3/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите число: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube)
+{
+    int length = cube.Length;
+    for (int i = 0; i < length; i++)
+    {
+        cube[i] = Convert.ToInt32(Math.Pow(i, 3));
+    }
+}
+
+void PrintArray(int[] coll)
+{
+    int count = coll.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(coll[i] + " ");
+    }
+}
+
+int[] array = new int[cube + 1];
+Cube(array);
+PrintArray(array);
